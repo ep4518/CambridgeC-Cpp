35e33038d5dc27c82a31744ce1d1be9a627257a6
ep4518 <edward.peterson18@imperial.ac.uk>
2024-03-08 12:09:21 +0000

    Add lab 6

--- lab6/graph.c
+++ lab6/graph.c
@@ -19,22 +19,34 @@ Node *node(int value, Node *left, Node *right) {
 
 int size(Node *node) { 
   /* TODO */
-  return 0;
+  if (node == NULL) return 0;
+  if (node->marked) return 0;
+  else {
+    node->marked = true;
+    return 1 + size(node->left) + size(node->right);
+  }
 }
 
 
 void unmark(Node *node) { 
   /* TODO */
+  if (node == NULL) return;
+  if (!node->marked) return;
+  node->marked = false;
+  unmark(node->left);
+  unmark(node->right);
 }
 
 bool path_from(Node *node1, Node *node2) {
   /* TODO */
-  return false;
+  if (node1 == NULL || node2 == NULL) return false;
+  else if (node1 == node2) return true;
+  return path_from(node1->left, node2) || path_from(node1->right, node2);
 }
 
 bool cyclic(Node *node) { 
   /* TODO */
-  return false;
+  return (path_from(node->left, node) || path_from(node->right, node));
 } 
 
 
@@ -42,10 +54,19 @@ bool cyclic(Node *node) {
 
 void get_nodes(Node *node, Node **dest) { 
   /* TODO */
+  if (node->marked || node == NULL) return;
+  node->marked = true;
+  *dest = node;
+  (*dest)++;
+  get_nodes(node->left, dest);
+  get_nodes(node->right, dest);
 }
 
-void graph_free(Node *node) { 
-  /* TODO */
+void graph_free(Node *node) {
+  /* TODO */ 
+  Node * nodes[size(node)];
+  /* pass a pointer into the nodes array to the get_nodes function */
+  Node **dest = nodes;
+  get_nodes(node, dest);
 }
-
 
