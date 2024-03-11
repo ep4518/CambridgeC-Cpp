0a008c806498e087e30c6b0fb2a6b7bfdc6f3cfd
ep4518 <edward.peterson18@imperial.ac.uk>
2024-03-11 11:26:27 +0000

    Add lab 7

--- lab7/re.c
+++ lab7/re.c
@@ -4,39 +4,80 @@
 
 arena_t create_arena(int size) { 
   /* TODO */
-  return NULL;
+  arena_t arena = malloc(sizeof(struct arena));
+  arena->size = size;
+  arena->current = 0;
+  arena->elts = malloc(size * sizeof(Regexp));
+  return arena;
 }
 
 void arena_free(arena_t a) { 
   /* TODO */
+  free(a->elts);
+  free(a);
 }
 
 Regexp *re_alloc(arena_t a) { 
   /* TODO */
-  return NULL;
+  if (a->current >= a->size) return NULL;
+  Regexp *r = &a->elts[a->current];
+  a->current++;
+  return r;
 }
 
 Regexp *re_chr(arena_t a, char c) { 
   /* TODO */
-  return NULL;
+  Regexp *r = re_alloc(a);
+  if (r == NULL) return NULL;
+  r->type = CHR;
+  r->data.chr = c;
+  return r;
 }
 
 Regexp *re_alt(arena_t a, Regexp *r1, Regexp *r2) { 
   /* TODO */
-  return NULL;
+  Regexp *r = re_alloc(a);
+  if (r == NULL) return NULL;
+  r->type = ALT;
+  r->data.pair.fst = r1;
+  r->data.pair.snd = r2;
+  return r;
 }
 
 Regexp *re_seq(arena_t a, Regexp *r1, Regexp *r2) { 
   /* TODO */
-  return NULL;
+  Regexp *r = re_alloc(a);
+  if (r == NULL) return NULL;
+  r->type = SEQ;
+  r->data.pair.fst = r1;
+  r->data.pair.snd = r2;
+  return r;
 }
 
 int re_match(Regexp *r, char *s, int i) { 
   /* TODO */
-  return -1;
+  switch (r->type) {
+  case CHR:
+    {
+    if (s[i] == r->data.chr) return i + 1;
+    else return -1;
+    }
+  case SEQ:
+    {
+    int j = re_match(r->data.pair.fst, s, i);
+    if (j >= 0) return re_match(r->data.pair.snd, s, j);
+    else return -1;
+    }
+  case ALT:
+    {
+    int j1 = re_match(r->data.pair.fst, s, i);
+    if (j1 >= 0) return j1;
+    else return re_match(r->data.pair.snd, s, i);
+    }
+  default:
+    return -1;
+  }
 }
-
-
 
 void re_print(Regexp *r) { 
   if (r != NULL) { 
@@ -69,8 +110,4 @@ void re_print(Regexp *r) {
   } else { 
     printf("NULL");
   }
-}    
-
-
-      
-  
+}
